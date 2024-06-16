04f66fdca2f3bc618fbbcd03cac95cfd359e13f2
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-16 22:16:10 +0200

    Adding the base functionality

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -5,7 +5,48 @@ void print(const char* str) {
     }
 }
 
+int strlen(const char* str) {
+    int len = 0;
+    while (str[len]) {
+        len++;
+    }
+    return len;
+}
+
+int countVowels(const char* str) {
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+            count++;
+        }
+    }
+    return count;
+}
+
+char charFromInt(int i) {
+    return (char)(i + '0');
+}
+
+void printInt(int i) {
+    char str[11];
+    int j = 0;
+    if (i < 0) {
+        print("-");
+        i = -i;
+    }
+    while (i > 0) {
+        str[j++] = charFromInt(i % 10);
+        i /= 10;
+    }
+    if (j == 0) {
+        str[j++] = '0';
+    }
+    for (int k = j - 1; k >= 0; --k) {
+        print(&str[k]);
+    }
+}
+
 extern "C" void main(){
-    print("Hello, World!");
+    printInt(countVowels("hello world"));
     return;
 }
