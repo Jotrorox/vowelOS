02852d8c803185e7c35d888c8baba0e7b10112e1
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-16 22:38:04 +0200

    Updating printing

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,7 +1,62 @@
+#define KEYBOARD_PORT 0x60
+
 void print(const char* str) {
     unsigned short* video_memory = (unsigned short*)0xb8000;
     for (int i = 0; str[i] != '\0'; ++i) {
         video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    }
+}
+
+void print(char* str) {
+    unsigned short* video_memory = (unsigned short*)0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    }
+}
+
+void print(char c) {
+    unsigned short* video_memory = (unsigned short*)0xb8000;
+    video_memory[0] = (video_memory[0] & 0xFF00) | c;
+}
+
+void print(int i) {
+    char str[11];
+    int j = 0;
+    if (i < 0) {
+        print("-");
+        i = -i;
+    }
+    while (i > 0) {
+        str[j++] = (char)(i % 10 + '0');
+        i /= 10;
+    }
+    if (j == 0) {
+        str[j++] = '0';
+    }
+    for (int k = j - 1; k >= 0; --k) {
+        print(str[k]);
+    }
+}
+
+void print(unsigned int i) {
+    char str[11];
+    int j = 0;
+    while (i > 0) {
+        str[j++] = (char)(i % 10 + '0');
+        i /= 10;
+    }
+    if (j == 0) {
+        str[j++] = '0';
+    }
+    for (int k = j - 1; k >= 0; --k) {
+        print(str[k]);
+    }
+}
+
+void printNewLine() {
+    unsigned short* video_memory = (unsigned short*)0xb8000;
+    for (int i = 80; i < 160; ++i) {
+        video_memory[i] = (video_memory[i] & 0xFF00) | ' ';
     }
 }
 
@@ -23,30 +78,8 @@ int countVowels(const char* str) {
     return count;
 }
 
-char charFromInt(int i) {
-    return (char)(i + '0');
-}
-
-void printInt(int i) {
-    char str[11];
-    int j = 0;
-    if (i < 0) {
-        print("-");
-        i = -i;
-    }
-    while (i > 0) {
-        str[j++] = charFromInt(i % 10);
-        i /= 10;
-    }
-    if (j == 0) {
-        str[j++] = '0';
-    }
-    for (int k = j - 1; k >= 0; --k) {
-        print(&str[k]);
-    }
-}
-
 extern "C" void main(){
-    printInt(countVowels("hello world"));
+    print(countVowels("hello world"));
+    
     return;
 }
